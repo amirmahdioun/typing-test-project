bbafb5c52a845b6e0bd32d74aa282ede3611fa9f
sadegh-mhf <s.mohamadifar1372@gmail.com>
2021-11-23 20:17:45 +0330

    updateTimer function updated

--- typing-test.js
+++ typing-test.js
@@ -29,7 +29,7 @@ function initializeTest({ timeLimit, text }) {
 
 function update() {
   if (!hasStarted) {
-    timer = setInterval(updateTimer, 1000);
+    timer = setInterval(updateTimer(timeLeft,timeElapsed,wpm), 1000);
     hasStarted = true;
   }
   typedCharacter++;
@@ -54,7 +54,7 @@ function updateWpm() {
   // TODO: Complete this function
 }
 
-function updateTimer() {
+function updateTimer(timeLeft,timeElapsed,wpm) {
     if(timeLeft > 0){
         timeElapsed ++
         timeLeft --
